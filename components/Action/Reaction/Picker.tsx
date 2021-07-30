4983d33da0691767625cdbf63e8e2f03d6feff7e
Orange <42256381+0-a-e@users.noreply.github.com>
2021-07-31 00:35:06 +0900

    カスタム絵文字ピッカーに検索ボックスを追加

--- components/Action/Reaction/Picker.tsx
+++ components/Action/Reaction/Picker.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { TouchableOpacity,Image,Text, View, ScrollView } from 'react-native';
+import { Input } from 'react-native-elements';
 import getMeta from '../../../data/Getmeta';
 
 const Picker = () => {
@@ -18,10 +19,17 @@ const Picker = () => {
             }))
         }
     return (
-        <View style={{width:"100%",alignItems: 'center'}}>
-        <ScrollView  contentContainerStyle={{flexDirection:'row',flexWrap: 'wrap',justifyContent:'space-between',paddingLeft:10,paddingRight:10,borderRadius:20,}} style={{borderRadius:20,width:"90%",backgroundColor:"#282a36"}}>
+        <View style={{width:"100%",alignItems: 'center',borderRadius:20,marginTop:20}}>
+        <View style={{width:"90%",backgroundColor:"#282a36",borderRadius:20}}>
+        <Input
+            inputContainerStyle={{borderBottomWidth: 0, borderRadius:50,padding:10,marginTop:10,backgroundColor:"#1b1d26"}}
+            style = {{color:"rgb(240,240,240)",width:"100%"}}
+                placeholder='絵文字を検索...'
+        />
+        <ScrollView contentContainerStyle={{flexDirection:'row',flexWrap: 'wrap',justifyContent:'space-between',paddingLeft:10,paddingRight:10,borderRadius:20,}} style={{width:"100%",marginTop:-15}}>
         { (meta) ?  py(meta): <Text>お待ちください...</Text>}
         </ScrollView>
+        </View>
         </View>
     );
 };
