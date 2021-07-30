214662420edef9b58435bb7a3db73384a5898d8b
Orange <42256381+0-a-e@users.noreply.github.com>
2021-07-31 00:22:39 +0900

    カスタム絵文字ピッカーを途中まで

--- components/Action/Reaction/Picker.tsx
+++ components/Action/Reaction/Picker.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TouchableOpacity,Image,Text } from 'react-native';
+import { TouchableOpacity,Image,Text, View, ScrollView } from 'react-native';
 import getMeta from '../../../data/Getmeta';
 
 const Picker = () => {
@@ -12,15 +12,17 @@ const Picker = () => {
 
         const py =(meta) => {
             return(meta.emojis.map(data => { 
-                return (<TouchableOpacity style={{backgroundColor:"red",width:60,height:60}} key={data.id} onPress={() => alert('Hello')}>
-                    <Image source={{uri:data.url}}  style={{ width: 60, height: 60 }}/>
+                return (<TouchableOpacity style={{width:55,height:55,borderRadius:15}} key={data.id} onPress={() => alert('Hello')}>
+                    <Image source={{uri:data.url}}  style={{ width: 55, height: 55 }}/>
                     </TouchableOpacity>)
             }))
         }
     return (
-        <>
+        <View style={{width:"100%",alignItems: 'center'}}>
+        <ScrollView  contentContainerStyle={{flexDirection:'row',flexWrap: 'wrap',justifyContent:'space-between',paddingLeft:10,paddingRight:10,borderRadius:20,}} style={{borderRadius:20,width:"90%",backgroundColor:"#282a36"}}>
         { (meta) ?  py(meta): <Text>お待ちください...</Text>}
-        </>
+        </ScrollView>
+        </View>
     );
 };
 export default Picker;
