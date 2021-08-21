cbbd9adef02530bbdeaf60f11c3f479eb14eb4bf
Orange <42256381+0-a-e@users.noreply.github.com>
2021-08-21 19:48:54 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View} from 'react-native';
+import Image from 'react-native-remote-svg';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{backgroundColor:"green",lineHeight:20
+      ,paddingBottom:3}}>
+        お <Image key="ddj" style={{width:20,height:20}} source={{uri: "https://www.kawaiifabric.com/images/product_images/large_img/solid-red-fabric-Robert-Kaufman-USA-Red-179485-1.JPG"}} />
+        ae <Image key="ddj" style={{width:20,height:20}} source={{uri: "https://upload.wikimedia.org/wikipedia/commons/f/ff/Solid_blue.svg"}} />
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
