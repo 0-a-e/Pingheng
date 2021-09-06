95475c75f921e88fe52c6089b47a21d0f88d9ae2
Orange <42256381+0-a-e@users.noreply.github.com>
2021-09-06 20:31:16 +0900

    ん

--- android/app/src/main/java/com/orange/pingheng/generated/BasePackageList.java
+++ android/app/src/main/java/com/orange/pingheng/generated/BasePackageList.java
@@ -9,7 +9,6 @@ public class BasePackageList {
     return Arrays.<Package>asList(
         new expo.modules.application.ApplicationPackage(),
         new expo.modules.constants.ConstantsPackage(),
-        new expo.modules.crypto.CryptoPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
